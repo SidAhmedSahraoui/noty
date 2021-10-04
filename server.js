b71572c8b553f698cf301bb6eca2332a1c3a47f4
sid ahmed <s.sahraoui@esi-sba.dz>
2021-10-04 17:47:10 +0100

    fix issus

--- server.js
+++ server.js
@@ -12,13 +12,10 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-}
+app.use(express.static("client/build"));
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+);
 
 const PORT = 5000;
-
 app.listen(PORT, () => console.log(`Server started, port: ${PORT}`));
