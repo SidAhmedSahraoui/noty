df7ff60dab22545d6fb2729c9ea03be26e7a68cc
Sid Ahmed Sahraoui <s.sahraoui@esi-sba.dz>
2022-07-16 06:57:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,10 +12,16 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-app.use(express.static("client/build"));
-app.get("*", (req, res) =>
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-);
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-const PORT = 5000;
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server started, port: ${PORT}`));
